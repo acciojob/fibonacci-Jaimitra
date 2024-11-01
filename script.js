0df745a4b69194723feacce1facba2c037d62488
Jaimitra <141642600+Jaimitra@users.noreply.github.com>
2024-11-01 11:05:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
 // your code here
+    let	count=2;
+	for(let i = 0,let j = 1;count <= num;count++){
+		let k=j;
+		j=i+j;
+		i=k;
+	}
+	console.log(j);
 }
 
 module.exports = fibonacci;
